925c54192c17098c7fbd8ee375185f48eff2b5d9
jmargielewski <kuba.margielewski@gmail.com>
2019-06-26 22:10:36 +0100

    fix class

--- src/components/projects/ProjectSummary.tsx
+++ src/components/projects/ProjectSummary.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ProjectSummary = () => {
   return (
-    <div className="cart z-depth-0 project-summary">
-      <div className="cart-conent grey-text text-darken-3">
-        <span className="cart-title">Project title</span>
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project title</span>
         <p>Posted by the Kuba</p>
         <p className="grey-text">3rd September, 2am</p>
       </div>
